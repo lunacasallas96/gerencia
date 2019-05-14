0ba4a87c1e10c7bbd0be6847b8752ae56100353d
Jose Luna <lunacasallas96@gmail.com>
2019-05-14 16:24:39 -0500

    mas cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        <center>
+        <img src="{{ asset('css/logo-San-Miguel.png') }}">
+        </center>
             <div class="card">
                 <div class="card-header">Inicio</div>
 
