57bf6f6608217dc683f68626fb343479f2709504
Jose Luna <lunacasallas96@gmail.com>
2019-05-14 20:03:48 -0500

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(EmpresasSeeder::class);
         $this->call(EscalaSeeder::class);
         $this->call(TipoCriterioSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
